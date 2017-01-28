c2fa02b89b82f38eec48f7d2770b8a22230999a8
wanglilong <wanglilong007@gmail.com>
2017-01-28 16:17:37 +0800

    get out cloud foundry connetor

--- src/main/java/com/exposure/common/config/CloudConfig.java
+++ src/main/java/com/exposure/common/config/CloudConfig.java
@@ -5,25 +5,26 @@ import java.util.Map;
 
 import javax.sql.DataSource;
 
-import org.springframework.cloud.Cloud;
-import org.springframework.cloud.CloudFactory;
-import org.springframework.cloud.config.java.AbstractCloudConfig;
-import org.springframework.cloud.service.PooledServiceConnectorConfig.PoolConfig;
-import org.springframework.cloud.service.keyval.RedisConnectionFactoryConfig;
-import org.springframework.cloud.service.relational.DataSourceConfig;
+//import org.springframework.cloud.Cloud;
+//import org.springframework.cloud.CloudFactory;
+//import org.springframework.cloud.config.java.AbstractCloudConfig;
+//import org.springframework.cloud.service.PooledServiceConnectorConfig.PoolConfig;
+//import org.springframework.cloud.service.keyval.RedisConnectionFactoryConfig;
+//import org.springframework.cloud.service.relational.DataSourceConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 
-@Configuration
-public class CloudConfig extends AbstractCloudConfig {
+//@Configuration
+public class CloudConfig {
+		//extends AbstractCloudConfig {
 
 //	@Bean
 //	public Cloud cloud() {
 //	  return new CloudFactory().getCloud();
 //	}
 	
-    @Bean
+/*    @Bean
     public DataSource inventoryDataSource() {
         PoolConfig poolConfig = new PoolConfig(5, 30, 3000);
         DataSourceConfig dbConfig = new DataSourceConfig(poolConfig, null);
@@ -37,5 +38,5 @@ public class CloudConfig extends AbstractCloudConfig {
       PoolConfig poolConfig = new PoolConfig(5, 30, 3000);
       RedisConnectionFactoryConfig redisConfig = new RedisConnectionFactoryConfig(poolConfig, properties);
       return connectionFactory().redisConnectionFactory("redis", redisConfig);
-    }
+    }*/
 }
